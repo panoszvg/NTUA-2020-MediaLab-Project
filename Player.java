32d49818723ec5f1fca4168ece7dd492b5046b09
Panagiotis Zevgolatakos <panoszevg@outlook.com>
2020-12-05 13:44:44 +0200

    Add basic Enemy AI in Player.java

--- Player.java
+++ Player.java
@@ -1,7 +1,10 @@
+import java.util.Random;
+
 public class Player {
     private String name;
     private int points;
     private int moves;
+    private boolean AIOption;
 
     /* Initalizes Player class with Name, Points
     (initially 0) and Moves (initially 40) */
@@ -9,6 +12,7 @@ public class Player {
         this.name = name;
         this.points = 0;
         this.moves = 40;
+        AIOption = false;
     }
 
     /* Prints Player Statistics - for debugging */
@@ -23,9 +27,29 @@ public class Player {
         this.moves--;
     }
 
-    /* Increase PLayer points */
+    /* Increase Player points */
     public void IncreasePoints(int points){
         this.points += points;
+    }
+
+    public boolean getAIOption(){
+        return this.AIOption;
+    }
+
+    /* Is used by the enemy/computer to choose which position to hit -
+    possiblePositions is given by main and is updated when this function resolves */
+    public int[] AIChoose(int[][] possiblePositions){
+        Random rand = new Random();
+        int[] ans = new int[2];
+        if(this.AIOption == false){
+            ans[0] = rand.nextInt(10);
+            ans[1] = rand.nextInt(10);
+        }
+        else{
+            ans[0] = possiblePositions[0][0];
+            ans[1] = possiblePositions[0][1];
+        }
+        return ans;
     }
 
 }
